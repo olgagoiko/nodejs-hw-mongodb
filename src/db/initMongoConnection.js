ae20bd0145d8ccf19ba6cf6e3c6b9dd050b9b382
Olga Goiko <olga.a10@ukr.net>
2024-11-18 23:22:48 +0200

    add

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,21 +1,14 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
-
-import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
-
-    await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=HW1`,
-    );
+    const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`;
+    await mongoose.connect(connectionString);
     console.log('Mongo connection successfully established!');
   } catch (error) {
-    console.log('Error while setting up mongo connection', error);
-    throw error;
+    console.error('Error connecting to MongoDB', error);
+    process.exit(1);
   }
 };
 
