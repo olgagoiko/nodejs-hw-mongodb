d3abeb0575b61ac045b226e51eedd6f705845137
Olga Goiko <olga.a10@ukr.net>
2024-11-27 20:59:58 +0200

    add updates to files

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+  } else {
+    next(err);
+  }
+};
+
+export default errorHandler;
