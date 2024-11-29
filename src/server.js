a4483dd56915dddfd285ae6f1d65758c9229ff1b
Olga Goiko <olga.a10@ukr.net>
2024-11-29 21:35:00 +0200

    add updates to server

--- src/server.js
+++ src/server.js
@@ -16,9 +16,9 @@ app.use((req, res, next) => {
   logger.info(`${req.method},${req.url}`);
   next();
 });
-app.use(notFoundHandler);
-app.use(errorHandler);
 
+app.use(errorHandler);
+app.use(notFoundHandler);
 export const setupServer = () => {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
